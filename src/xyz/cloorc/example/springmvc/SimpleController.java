19ab24788b6c638272784982d323f7e47b0aeaf4
zhangh <zhangh@dtdream.com>
2016-05-26 16:42:07 +0800

    simple update

--- src/xyz/cloorc/example/springmvc/SimpleController.java
+++ src/xyz/cloorc/example/springmvc/SimpleController.java
@@ -1,24 +1,32 @@
 package xyz.cloorc.example.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
 import xyz.cloorc.example.springmvc.utils.HttpHelper;
 
 @Controller
 public class SimpleController {
 
-    @ResponseBody
-    @RequestMapping(value="/hello", method=RequestMethod.GET)
-    public String hello () {
-        return ("hello, we r in java");
-    }
+	@ResponseBody
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		return ("hello, we r in java");
+	}
 
-    @ResponseBody
-    @RequestMapping(value = "/proxy", method = RequestMethod.GET)
-    public String proxy (@RequestParam(value = "target")String target) {
-        return HttpHelper.get(target, null, null);
-    }
+	@RequestMapping(value = "/redirect", method = RequestMethod.GET)
+	public ModelAndView redirect(@PathVariable long id) {
+		return Redirect;
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/proxy", method = RequestMethod.GET)
+	public String proxy(@RequestParam(value = "target") String target) {
+		return HttpHelper.get(target, null, null);
+	}
 }
